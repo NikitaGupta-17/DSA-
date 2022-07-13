9ebbb67df6ae74d12a3ef6163b6cfa755a0a7b32
NikitaGupta-17 <75840623+NikitaGupta-17@users.noreply.github.com>
2022-07-13 18:57:27 +0530

    Update StockBuyAndSell.cpp

--- Array/StockBuyAndSell.cpp
+++ Array/StockBuyAndSell.cpp
@@ -7,28 +7,26 @@ int main()
     cout << "Enter the size Of array : ";
     cin >> size;
     int arr[size];
-    cout <<"Enter the arrray elements : ";
+    cout <<"Enter the array elements : ";
     for(i=0; i<size ; i++)
     {
         cin >> arr[i];
     }
-    int buy ,sell,j,temp=0;
-    for(i=0;i<size;)
-    {
-        buy=arr[i];
-        for(j=i+1;j<size;j++)
-        {
-            if(arr[j]>arr[i] && arr[j]>arr[j+1])
-            {
-                sell = arr[j];
-                cout<<"Buy at "<<i<<" and sell at "<<j<<endl;
-                i=j+1;
-                temp++;
-                break;
-            }
-        }
+    int temp=0;
+    i=0;
+    while (i < size - 1) {
+        while ((i < size - 1) && (arr[i + 1] <= arr[i]))
+            i++;
+        if (i == size - 1)
+            break;
+        int buy = i++;
+        while ((i < size) && (arr[i] >= arr[i - 1]))
+            i++;
+        int sell = i - 1;
+        cout << "Buy on day: " << buy<< "\t Sell on day: " << sell << endl;
+        temp++;
     }
-    if (temp == 0)
+    if (temp==0)
     cout << "0";
     return 0;
 }
